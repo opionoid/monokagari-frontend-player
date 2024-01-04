9fe0b0d6467588dca03fd74c6acdc1c7607b4eb2
Opion <kindled0kindred@icloud.com>
2024-01-04 18:37:37 +0900

    feat: i18n config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,16 @@ import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
+  // Partial SSR with Deno Deploy
   output: "hybrid",
   adapter: deno(),
+
+  // i18n
+  i18n: {
+    defaultLocale: "ja",
+    locales: ["en", "ja"],
+  },
+  routing: {
+    prefixDefaultLocale: true,
+  },
 });
